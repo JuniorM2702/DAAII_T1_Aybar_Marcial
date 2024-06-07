e5c28e5e03183d176fcc6b1554b71b8df25f6b05
JuniorM2702 <Junior_aybar27@hotmail.com>
2024-06-07 11:26:31 -0500

    Incio

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_AybarNapan/DaaiiT1AybarNapanApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_AybarNapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1AybarNapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1AybarNapanApplication.class, args);
+	}
+
+}
